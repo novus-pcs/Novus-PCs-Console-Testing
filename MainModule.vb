5df0ffa8d78cb3e8e9a7e5d9f624e85b4ef2d347
cludlow-novuspcs <52876749+cludlow-novuspcs@users.noreply.github.com>
2019-07-14 00:38:28 -0700

    Commit #1

---
+++ MainModule.vb
@@ -0,0 +1,28 @@
+﻿Module MainModule
+    Dim OSMajor As String
+    Sub Main()
+        'Declaration Of Variables
+
+        Dim OSName = My.Computer.Info.OSFullName
+        Dim OSVer = My.Computer.Info.OSVersion
+        Dim OSArch As String
+        '
+        'For Windows 10 ONLY convert build number to scheme ex. 1903
+        If OSVer = "10.0.18362.0" Then
+            OSVer = "1903"
+        Else OSVer = My.Computer.Info.OSVersion
+        End If
+        '
+        'Determine if x64 or x86
+        If System.Environment.Is64BitOperatingSystem = True Then
+            OSArch = "x64"
+        Else
+            OSArch = "x86"
+        End If
+        '
+        'Write results to command line
+        Console.WriteLine($"Your Computer Is Running : {OSName} {OSVer} {OSArch}")
+        'Break
+    End Sub
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Novus PCs Console Testing")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Novus PCs Console Testing")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("cad6a9fe-ab84-4d42-813f-5cfa34ef16ff")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
